6ac31936221c47cd4fe6d974856adc5f514e794a
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2018-12-07 15:16:11 +0800

    non linear terms

--- Practice/linear_regression.R
+++ Practice/linear_regression.R
@@ -23,10 +23,48 @@ fit3 = lm(medv ~ ., data = Boston)
 fit3
 summary(fit3)
 par(mfrow = c(2,2))
-plot(fit3)
+#plot(fit3)
 fit4 = update(fit3, ~. -age -indus)
 summary(fit4)
 
 
 ### Non linear terms and interactions
+fit5 = lm(medv ~ lstat*age, Boston)
+summary(fit5)
+plot(fit5)
 
+fit6 = lm(medv~lstat + I(lstat^2),Boston)
+summary(fit6)
+attach(Boston)
+par(mfrow = c(1,1))
+plot(medv~lstat)
+points(lstat, fitted(fit6), col="red", pch = 20) # fitted values from the model, plotting character to be 20
+## Easier way of fitting polynomial functions
+
+fit7 = lm(medv~poly(lstat,4))
+points(lstat, filled(fit7), col = "yellow", pch = 20)
+plot(1:20, 1:20, pch = 1:20, cex=2)
+
+#Qualitative Predictors 
+fix(Carseats)
+summary(Carseats)    
+fit8 = lm(Sales~.+Income:Advertising+Age:Price, Carseats)
+summary(fit8)
+plot(fit8)
+contrasts(Carseats$ShelveLoc)
+regplot=function(x,y){
+  fit = lm(y~x)
+  plot(x,y)
+  abline(fit,col = "red")
+}
+
+attach(Carseats)
+regplot(Price, Sales)
+
+regplot(x,y,...){ # ... means unnamed arguments
+  fit = lm(y~x)
+  plot(x,y, ...)
+  abline(fit, col = "red")
+}
+
+regplot(Price, Sales, xlab = "Price", ylab = "Sales", col = "blue", pch = 20)
